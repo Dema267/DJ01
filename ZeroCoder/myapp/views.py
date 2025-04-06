be778902527e29a82bc307844e564715a7b51bdc
Dema267 <demademin292@gmail.com>
2025-04-06 10:37:06 +0300

    add

--- ZeroCoder/myapp/views.py
+++ ZeroCoder/myapp/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_page(request):  # Новый view для главной страницы
+    return HttpResponse("Главная страница. Перейдите на /data/ или /test/")
 
 def data_page(request):
     return HttpResponse("Это страница DATA. Привет, Django!")
 
 def test_page(request):
     return HttpResponse("Это страница TEST. Работает!")
-
-
